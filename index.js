3fb28ac5c36165a32f59e0524bd1559c1763b663
unknown <alfilouis@gmail.com>
2020-04-12 16:28:23 +0300

    Bux fix in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-express.use(express.static('public'))
+app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
