82477f2d7fc4e3ab61b6cabf60994dfebe509bd2
unknown <alfilouis@gmail.com>
2020-04-19 21:31:17 +0300

    The other half of the last commit. I don't really know what happened.

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const path = require("path")
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const ejsview = (url, filename=url) => ['/'+url, (req, res) => {
+	res.render(filename + '.ejs')
+}]
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'views'))
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
+  res.render('index.ejs')
 })
+app.get(...ejsview('notice'))
+
 app.listen(PORT, err => {
 	if(err) console.log(err)
 	else console.log(`Server listening on port ${PORT}`)
