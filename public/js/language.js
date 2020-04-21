571d47a5c0fc4c62b7235c2dd90f22ba4f731026
unknown <alfilouis@gmail.com>
2020-04-21 14:48:34 +0300

    Rebuilt the lang drop

--- public/js/language.js
+++ public/js/language.js
@@ -1,17 +1,40 @@
 const nameFor = { ro: "Română", en: "English" }
-var langDrop, selected
+
+var displayed, langDrop, selected
+
 window.onload = function() {
-	langDrop = document.querySelector("#languages")
-  selected = document.querySelector("#selected-lang")
+  displayed = document.querySelector("#displayed-lang")
+  langDrop = document.querySelector("#languages")
+  selected = document.querySelector("button.selected-lang")
+
+  displayed.addEventListener('click', e => dropdownLang())
+  displayed.addEventListener('blur', langBlur)
+  document.querySelectorAll("button.lang").forEach(btn => {
+  	btn.addEventListener('click', function(e) {
+  		langShort = this.getAttribute("data-lang")
+  		changeLang(langShort, this)
+  	})
+  	btn.addEventListener('blur', langBlur)
+  })
 }
 
-function changeLang(lang) { 
+function changeLang(lang, btn) { 
 	document.body.className = lang
-	window.selected.innerHTML = nameFor[lang]
-	langDrop.hidden = true
+	window.displayed.innerHTML = nameFor[lang]
+	window.selected.classList.remove("selected-lang")
+	btn.classList.add("selected-lang")
+	window.selected = btn
+	dropdownLang(false)
 }
 
 function dropdownLang(open) {
 	open = open != undefined ? open : langDrop.hidden
 	langDrop.hidden = !open
+	if(open) selected.focus()
 }
+
+function langBlur(e) {
+	if(e.relatedTarget?.classList.contains("lang") || e.relatedTarget == displayed) return;
+	dropdownLang(false)
+}
+
