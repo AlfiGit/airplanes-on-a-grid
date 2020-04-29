20adcf90262f8375557f3ef1a7e0f7b01a59adf1
unknown <alfilouis@gmail.com>
2020-04-29 17:34:16 +0300

    Worked on notice.ejs

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const path = require("path")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const ejsview = (url, filename=url) => ['/'+url, (req, res) => {
-	res.render(filename + '.ejs')
+const ejsview = (url, filename=url, params) => ['/'+url, (req, res) => {
+	Object.keys(params).map(key => params[key] = req.query[params[key]])
+	res.render(filename + '.ejs', params)
 }]
+var language = "en"
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'views'))
@@ -14,7 +16,10 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
-app.get(...ejsview('notice'))
+app.get('/notice', (req, res) => {
+	res.render('notice.ejs', {language: req.query.lang})
+})
+//app.get(...ejsview('notice', {language: 'lang'}))
 
 app.listen(PORT, err => {
 	if(err) console.log(err)
